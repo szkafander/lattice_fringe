415b64b0030498e2fd066b9dce6cbf6949d6e077
amarton <marton.aczel@gmail.com>
2022-12-04 17:59:29 +0100

    added resize test - wip

--- python/tests/test_grids.py
+++ python/tests/test_grids.py
@@ -1,1 +1,17 @@
 # test grid
+# python -m pytest
+# python -m ==> run something as a script
+
+from lattice_fringe.grids import spatial_grid as sg
+import numpy as np
+
+sg_grid = sg.SpatialGrid(np.ndarray([1, 2, 3 ,4 ,5]), np.ndarray([1, 2, 3 ,4 ,5]), None)
+
+def test_test():
+    assert 1 == 1
+
+# MARK: Spatial grid tests
+
+def test_resize():
+    sg_grid_2 = sg_grid.resize(1, (10, 10))
+    assert 1 == 1
