2ed385a71b8fb74357708b99c7d07814762a0bb6
szkafander <toth.pala@gmail.com>
2022-12-04 10:49:49 -0600

    frequency grid transform

--- python/lattice_fringe/grids/frequency_grid.py
+++ python/lattice_fringe/grids/frequency_grid.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import matplotlib.pyplot as pl
 import numpy as np
 
-from lattice_fringe.interfaces import Grid, SpatialUnit
+from lattice_fringe.interfaces import Grid, FrequencyUnit
 from lattice_fringe.utils import get_composite
 
 from typing import TYPE_CHECKING
@@ -18,18 +18,17 @@ class FrequencyGrid(Grid):
             self,
             coords_0: np.ndarray,
             coords_1: np.ndarray,
-            unit: SpatialUnit
+            unit: FrequencyUnit
     ) -> None:
         super(FrequencyGrid, self).__init__(coords_0, coords_1, unit)
 
     def transform(self) -> SpatialGrid:
-        from lattice_fringe.grids.spatial_grid import SpatialGrid
-        width = self.width
-        height = self.height
-        return FrequencyGrid(
-            np.arange(-width / 2, width / 2) / self.delta_0 / width,
-            np.arange(-height / 2, height / 2) / self.delta_1 / height,
-            self.unit.transform()
+        xr = self.extent_0[1] - self.extent_0[0]
+        yr = self.extent_1[1] - self.extent_1[0]
+        return SpatialGrid(
+            np.linspace(0, xr, len(self.x_axis)),
+            np.linspace(0, yr, len(self.y_axis)),
+            axes=self.unit.transform()
         )
 
     def plot(self) -> None:
