f083857d789babc50328fea5699ca88e24565e87
amarton <marton.aczel@gmail.com>
2022-12-15 22:22:58 +0100

    assert parameter combination idea (sketch)

--- python/tests/test_grids.py
+++ python/tests/test_grids.py
@@ -18,15 +18,29 @@ def _x0():
 def _x1():
     return np.arange(0, 7, 2)
 
-
 @pytest.fixture(name="bg")
 def basic_grid(_x0, _x1):
     return SpatialGrid(_x0, _x1, "unit")
 
+# TODO: Check this idea to solve the parameter combinations problem (sketch)
+def test_grid_axes():
+    errors = []
+
+    x0_arr = [[np.arange(2), [0, -1]],
+              [np.arange(5), [0, -1, 2, 3, 4]],
+              [np.arange(6), [0, -1, 2, 3, 4, 5]]]
+
+    for elem in x0_arr:
+        sg = SpatialGrid(elem[0], np.arange(1), "unit")
+        
+        if not np.array_equal(sg.axis_0, elem[1]):
+            errors.append(str(sg.axis_0)+"is not equal to "+str(elem[1]))
+    
+    assert not errors, "errors occured:\n{}".format("\n".join(errors))
+
 
 def test_grid_names(bg):
     assert (bg.name_0, bg.name_1) == ("x", "y")
-
 
 def test_grid_axis_0(bg, request):
     print(request)
